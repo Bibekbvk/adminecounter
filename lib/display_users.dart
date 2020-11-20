8ce43cd1d0d34b1ba022401017492da11911bd0b
Raiesh Ghimire <raieshg@gmail.com>
2020-11-20 16:21:11 +0545

    recieved seat number

--- lib/display_users.dart
+++ lib/display_users.dart
@@ -3,6 +3,7 @@ import 'package:adminecounter/display_booking.dart';
 import 'package:adminecounter/user_model.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 class ShowUsers extends StatefulWidget {
   final String service;
 
@@ -19,23 +20,30 @@ class _ShowUsersState extends State<ShowUsers> {
     return Scaffold(
       appBar: AppBar(),
       body: StreamBuilder(stream:db.getuserdetailsfirebase(widget.service) , builder: (context, snapshot){
+        print(snapshot.data);
         if(snapshot.hasData){
           return ListView.builder(itemCount: snapshot.data.length,itemBuilder: (BuildContext context,int index){
              String contact=snapshot.data[index].contact;
              String name=snapshot.data[index].name;
              String ticket_for=snapshot.data[index].ticket_for;
              String vehicle_id=snapshot.data[index].vehicle_id;
+             String seat_number=snapshot.data[index].seat_number;
 
 
-            UserModel moversmodel = UserModel(contact: contact,name: name,ticket_for: ticket_for,vehicle_id: vehicle_id);
+            UserModel moversmodel = UserModel(contact: contact,name: name,ticket_for: ticket_for,vehicle_id: vehicle_id,seat_number: seat_number);
 
             return Column(
 
               children: [
-                Text("$name"),
-                Text("$contact"),
-                Text('${snapshot.data[index].vehicle_number}'),
-                Text("$vehicle_id"),
+                Text("Name : $name"),
+                Text("Contact Number : $contact"),
+                Text('Vehicle Number : ${snapshot.data[index].vehicle_number}'),
+                Text("Departure Date : ${snapshot.data[index].ticket_for}"),
+
+               Text("Seat Number : ${snapshot.data[index].seat_number}"),
+
+
+
                 TextField(
                   controller: _ticketController,
 
@@ -45,10 +53,17 @@ class _ShowUsersState extends State<ShowUsers> {
                       context,
                       MaterialPageRoute(
                           builder: (context) => ShowVehicles(vehicle_id:vehicle_id)));*/
+                 if(widget.service=="User Booking"){
                   FirebaseFirestore.instance.collection('${widget.service}').doc('${snapshot.data[index].transaction_id}').update({
                     'link':"${_ticketController.text}",
                     'status':'successfull',
-                  });
+                  });}
+                 else{
+                   FirebaseFirestore.instance.collection('${widget.service}').doc('${snapshot.data[index].transaction_id}').update({
+
+                     'status':'successfull',
+                   });
+                 }
 
 
                 },child: Text("Update ticket"),)
